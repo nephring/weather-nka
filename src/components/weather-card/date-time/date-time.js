6c3437b62b3aebe943d485de6aefa42cdb9fde51
falkowski.maciej <maciej.nedza-falkowski@comarch.pl>
2018-07-02 21:34:26 +0200

    display date and time

--- src/components/weather-card/date-time/date-time.js
+++ src/components/weather-card/date-time/date-time.js
@@ -1,12 +1,56 @@
 import React from 'react'
+import moment from 'moment'
 
 import styles from './date-time.module.scss'
 
-const DateTime = () => (
-  <div className={styles.container}>
-    <h2 className={styles.time}>14:00:23</h2>
-    <p className={styles.date}>Thursday, May 19, 2016</p>
-  </div>
-)
+class DateTime extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { date: moment() }
+  }
+
+  getTime = () => {
+    return (
+      `${moment().format('HH')}:` +
+      `${moment().format('mm')}:` +
+      `${moment().format('ss')}`
+    )
+  }
+
+  getDate = () => {
+    return (
+      `${moment().format('dddd')}, ` +
+      `${moment().format('MMMM')} ` +
+      `${moment().format('D')}, ` +
+      `${moment().format('YYYY')}`
+    )
+  }
+
+  changeDate () {
+    this.setState({ date: moment() })
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => this.changeDate(), 1000)
+  }
+
+  componentWillMount () {
+    clearInterval(this.timer)
+  }
+
+  render () {
+    return (
+      <div className={styles.container}>
+        <h2 className={styles.time}>
+          {this.getTime()}
+        </h2>
+        <p className={styles.date}>
+          {this.getDate()}
+        </p>
+      </div>
+    )
+  }
+}
 
 export default DateTime
--- src/utils/getLocalDate.js
+++
@@ -1,9 +0,0 @@
-export const getLocalDate = () => {
-  const date = new Date()
-  const day = date.toLocaleDateString(window.navigator.language, { weekday: 'long' })
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-
-  return `${day}, ${hours}:${minutes}`
-}
-
