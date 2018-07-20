d560111c09799ca9247fef0c991c5a93e1b2fbb2
falkowski.maciej <maciej.nedza-falkowski@comarch.pl>
2018-07-20 14:06:13 +0200

    main card mobile view

--- src/components/weather-card/weather-card.js
+++ src/components/weather-card/weather-card.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import LocationName from './location-name/location-name'
 import DateTime from './date-time/date-time'
-import Temperature from './temperature/temperature'
 import Details from './details/details'
 
 import styles from './weather-card.module.scss'
@@ -11,9 +10,8 @@ const WeatherCard = () => (
   <div className={styles.card}>
     <div className={styles.main}>
       <LocationName />
-      <div className={styles.mainRowWrapper}>
+      <div className={styles.timeContainer}>
         <DateTime />
-        <Temperature />
       </div>
     </div>
     <div className={styles.details}>
