83033e83072b7b6d6fd3093812c820302bb2d2ae
nephring <nephring@gmail.com>
2018-10-01 20:16:43 +0200

    move API_KEY to env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,5 @@
-const API_KEY = '5a6b5c00121f2304a0a79426028fa3e1'
-
 export const getData = async (city, countryISO, type) => {
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/${type}?q=${city},${countryISO}&units=metric&appid=${API_KEY}`)
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/${type}?q=${city},${countryISO}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
   const response = await api_call.json()
   return response
 }
