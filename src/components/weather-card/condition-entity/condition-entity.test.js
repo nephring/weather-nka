3e75ff65ef66233ffcbd211e2fe12c1f469b60f7
falkowski.maciej <maciej.nedza-falkowski@comarch.pl>
2018-10-19 16:39:56 +0200

    add condition entity test

---
+++ src/components/weather-card/condition-entity/condition-entity.test.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { shallow } from "enzyme"
+
+import ConditionEntity from "./condition-entity"
+
+const FakeIconComponent = () => <svg>icom here</svg>
+
+describe("condition-entity", () => {
+  it("renders value prop when passed in", () => {
+    const value = "10 m/s"
+    const wrapper = shallow(<ConditionEntity value={value} />)
+
+    expect(wrapper.find("span").text()).toBe("10 m/s")
+  })
+
+  it("renders value prop as jsx when passed in", () => {
+    const value1 = "10&deg;"
+    const value2 = "20&deg;"
+    const wrapper = shallow(
+      <ConditionEntity
+        value={
+          <div className="container">
+            <span>{value1}</span>
+            <span>{value2}</span>
+          </div>
+        }
+      />
+    )
+    const container = wrapper.find('.container')
+    expect(container.length).toEqual(1)
+    expect(container.find('span').at(0).text()).toBe(value1)
+    expect(container.find('span').at(1).text()).toBe(value2)
+  })
+
+  it("renders iconComponent prop when passed in", () => {
+    const wrapper = shallow(
+      <ConditionEntity iconComponent={<FakeIconComponent />} />
+    )
+    expect(wrapper.find(FakeIconComponent).exists()).toBe(true)
+  })
+})
