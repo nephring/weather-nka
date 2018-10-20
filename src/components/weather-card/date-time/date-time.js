9f87e44df3e938a7c2b5d47d444a8b91e21a8eb2
nephring <nephring@gmail.com>
2018-10-20 22:24:37 +0200

    move function from component to shared function file

--- src/components/weather-card/date-time/date-time.js
+++ src/components/weather-card/date-time/date-time.js
@@ -1,39 +1,8 @@
 import React from "react";
-import moment from "moment-timezone";
 import PropTypes from "prop-types";
 
+import { getDate, getTime } from "../../../shared/utils";
 import styles from "./date-time.scss";
-
-export const getDate = timezone => {
-  return (
-    `${moment()
-      .tz(`${timezone}`)
-      .format("dddd")}, ` +
-    `${moment()
-      .tz(`${timezone}`)
-      .format("MMMM")} ` +
-    `${moment()
-      .tz(`${timezone}`)
-      .format("D")}, ` +
-    `${moment()
-      .tz(`${timezone}`)
-      .format("YYYY")}`
-  );
-};
-
-export const getTime = timezone => {
-  return (
-    `${moment()
-      .tz(`${timezone}`)
-      .format("HH")}:` +
-    `${moment()
-      .tz(`${timezone}`)
-      .format("mm")}:` +
-    `${moment()
-      .tz(`${timezone}`)
-      .format("ss")}`
-  );
-};
 
 class DateTime extends React.Component {
   constructor(props) {
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,43 @@
+import moment from "moment-timezone";
+
 export const isDay = time => {
-  return time > "05:00:00" && time < "21:00:00"
-}
+  return time > "05:00:00" && time < "21:00:00";
+};
+
+export const getDate = timezone => {
+  return (
+    `${moment()
+      .tz(`${timezone}`)
+      .format("dddd")}, ` +
+    `${moment()
+      .tz(`${timezone}`)
+      .format("MMMM")} ` +
+    `${moment()
+      .tz(`${timezone}`)
+      .format("D")}, ` +
+    `${moment()
+      .tz(`${timezone}`)
+      .format("YYYY")}`
+  );
+};
+
+export const getTime = timezone => {
+  return (
+    `${moment()
+      .tz(`${timezone}`)
+      .format("HH")}:` +
+    `${moment()
+      .tz(`${timezone}`)
+      .format("mm")}:` +
+    `${moment()
+      .tz(`${timezone}`)
+      .format("ss")}`
+  );
+};
+
+export const getFutureDay = (day, timezone) => {
+  return moment(new Date())
+    .add(day, "days")
+    .tz(`${timezone}`)
+    .format("dddd");
+};
