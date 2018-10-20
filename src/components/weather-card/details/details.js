90f2e78d7eb98a3ab641d6c3000400ea98db6235
nephring <nephring@gmail.com>
2018-10-20 22:33:53 +0200

    delete unused import

--- src/components/weather-card/details/details.js
+++ src/components/weather-card/details/details.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import moment from "moment-timezone";
 import * as _ from "lodash";
 
 import ConditionEntity from "./../condition-entity/condition-entity";
