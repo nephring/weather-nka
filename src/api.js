1be93c49d80405e13fbe7871124bea6f5e442d03
nephring <nephring@gmail.com>
2018-10-21 16:08:57 +0200

    handle api call with axios

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
+import axios from "axios";
+
 export const getData = async (city, countryISO, type) => {
-  const api_call = await fetch(
-    `http://api.openweathermap.org/data/2.5/${type}?q=${city},${countryISO}&units=metric&appid=${
-      process.env.API_KEY
-    }`
-  );
-  const response = await api_call.json();
-  return response;
+  try {
+    const call = await axios.get(
+      `http://api.openweathermap.org/data/2.5/${type}?q=${city},${countryISO}&units=metric&appid=${
+        process.env.API_KEY
+      }`
+    );
+
+    const data = await call.data;
+    return data;
+  } catch (e) {
+    throw Error(e);
+  }
 };
 
 export default getData;
